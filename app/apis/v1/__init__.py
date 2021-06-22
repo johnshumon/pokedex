84336a2fee7b6ad8c2f9ebf1b9d5cfe433798312
Abu Shumon <abu.shumon@qvik.fi>
2021-06-22 12:59:50 +0100

    added endpoint to get a pokemon

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -3,6 +3,8 @@
 from fastapi import APIRouter
 
 from app.apis.v1.handlers import home
+from app.apis.v1.handlers import pokemon
 
 api_router = APIRouter()
 api_router.include_router(home.router, prefix="/home")
+api_router.include_router(pokemon.router, prefix="/pokemon/{pokemon_name}")
---
+++ app/apis/v1/handlers/pokemon.py
@@ -0,0 +1,42 @@
+"""Pokemon module"""
+
+from typing import Any
+
+from fastapi import APIRouter
+
+from app.core.config import settings
+from app.utils.async_client import client
+
+router = APIRouter()
+
+
+@router.get("")
+async def get_pokemon(pokemon_name: str) -> Any:
+    """Handler for getting details of a pokemon"""
+
+    # gets pokemon details from pokemon endpoint
+    POKEMON_BASE_API = settings.Config.POKEMON_BASE_API
+    POKEMON_URL = f"{POKEMON_BASE_API}/{pokemon_name}"
+    pokemon_api_res = await client.get(POKEMON_URL)
+    pokemon_json = pokemon_api_res.json()
+
+    # gets pokemon's species details from the species
+    # url extracted from previous call
+    POKEMON_SPECIES_URL = pokemon_json["species"]["url"]
+    species_api_res = await client.get(POKEMON_SPECIES_URL)
+    species_json = species_api_res.json()
+
+    # set values for result
+    name = pokemon_json["name"]
+    description = species_json["flavor_text_entries"][0]["flavor_text"]
+    habitat = species_json["habitat"]["name"]
+    is_legendary = species_json["is_legendary"]
+
+    result = {
+        "name": name,
+        "description": description,
+        "habitat": habitat,
+        "isLegendary": is_legendary
+    }
+
+    return result
