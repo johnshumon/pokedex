e0b7958bdcb2cc9c5e23e92a250e3054b9cb1527
Abu Shumon <abu.shumon@qvik.fi>
2021-06-24 16:09:25 +0100

    minor text updated

--- app/apis/v1/handlers/home.py
+++ app/apis/v1/handlers/home.py
@@ -1,12 +1,12 @@
 """Home handler module"""
 
-from typing import Any
+from typing import Dict
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
 @router.get("")
-async def read_root() -> Any:
+async def read_root() -> Dict:
     """Main handler for the home path"""
-    return {"message": "Hello FastAPI!"}
+    return {"message": "Hello Pokemon lover!"}
